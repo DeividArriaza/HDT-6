8026f9872e34dcd00a314d2eb95eca0d40788f41
DeividArriaza <lop24730@uvg.edu.gt>
2025-03-11 16:46:57 -0600

    Se crea la interfaz de la fábrica de los Maps

---
+++ MapFactory.java
@@ -0,0 +1,8 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+
+public interface MapFactory{
+    Map<String, Pokemon> createMap();
+}
